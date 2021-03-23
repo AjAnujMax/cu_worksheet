6b0be7dade11ca565716ecec44fdb36749637f60
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-23 12:23:15 +0530

    completed worksheet-3

--- WT_PHP_worksheet_3.php
+++ WT_PHP_worksheet_3.php
@@ -31,6 +31,8 @@ for($a=0; $a< 10; $a++)
   
 <!--   Write a PHP script using nested for loop that creates a chess board as shown below. Use table width="270px" and take 30px as cell height and width.
 <table width="270px" cellspacing="0px" cellpadding="0px" border="1px">
+
+
 <!-- cell 270px wide (8 columns x 60px) --> -->
 <?php
       for($row=1;$row<=8;$row++)
