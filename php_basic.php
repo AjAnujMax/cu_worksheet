ef6046997fccb817a4c9306aeb1e474506e55196
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-27 01:10:25 +0530

    hello world

---
+++ php_basic.php
@@ -0,0 +1,15 @@
+<!--______________________________ BASIC codes from start_____________________________________ -->
+<!-- PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages. -->
+
+
+<!-- ___________hello world_______________________________ -->
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html> 
