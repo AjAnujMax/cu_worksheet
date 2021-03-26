f93703399febc396d229d4f772b2cb843c9dadc5
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-27 02:15:19 +0530

    hello world using var in table

--- php_basic.php
+++ php_basic.php
@@ -5,11 +5,47 @@
 <!-- ___________hello world_______________________________ -->
 <!DOCTYPE html>
 <html>
+<link href="table.css" rel="stylesheet" type="text/css">
 <body>
+<table width="1429" height="606" border="1"> 
+<tbody>
+    <tr>
+      <th width="154" height="120" style="text-align: center">ad</th>
+      <th colspan="3" style="text-align: center; font-size: 36px;">PHP</th>
+      <th width="157" style="text-align: center">ad</th>
+    <tr>
+      <th rowspan="5" style="text-align: center">&nbsp;</th>
+      <th width="417" height="48" style="text-align: center">content</th>
+      <th width="460" style="text-align: center">php code </th>
+      <th width="329" style="text-align: center">Detail code</th>
+      <th rowspan="5" style="text-align: center">&nbsp;</th>
+    <tr>
+      <th style="text-align: center">hello</th>
+      <th style="text-align: center">
+<?php
+$txt = "PHP";
+echo "I AM STARTing with $txt!";
+?>
+		&nbsp;</th>
+      <th style="text-align: center">&nbsp;</th>
+    <tr>
+      <th style="text-align: center">&nbsp;</th>
+      <th style="text-align: center">&nbsp;</th>
+      <th style="text-align: center">&nbsp;</th>
+    <tr>
+      <th style="text-align: center">&nbsp;</th>
+      <th style="text-align: center">&nbsp;</th>
+      <th style="text-align: center">&nbsp;</th>
+    <tr>
+      <th style="text-align: center">&nbsp;</th>
+      <th style="text-align: center">&nbsp;</th>
+      <th style="text-align: center">&nbsp;</th>
+</table>
 
 <?php
-echo "My first PHP script!";
+$txt = "PHP";
+echo "I love $txt!";
 ?>
 
 </body>
-</html> 
+</html>> 
