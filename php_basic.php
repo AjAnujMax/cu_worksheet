b28f52f8799164bb13f841da40e00d4cb5b1bac6
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-29 01:12:58 +0530

    break continue

--- php_basic.php
+++ php_basic.php
@@ -373,8 +373,35 @@ foreach ($colors as $value) {
   echo "$value <br>";
 }
 ?>
+  <?php
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $val) {
+  echo "$x = $val<br>";
+}
+?>
+<!--    _______________________break continue-->
+  <?php
+for ($x = 0; $x < 10; $x++) {
+  if ($x == 4) {
+    break;
+  }
+  echo "The number is: $x <br>";
+}
+?>
   
-  
+  <?php
+$x = 0;
+
+while($x < 10) {
+  if ($x == 4) {
+    $x++;
+    continue;
+  }
+  echo "The number is: $x <br>";
+  $x++;
+}
+?>
   
   </body>
 </html> 
