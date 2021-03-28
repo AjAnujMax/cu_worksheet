5b9c511fdd87b0e0d2fcf4e59b043528f22161b2
Anuj <56772154+AjAnujMax@users.noreply.github.com>
2021-03-28 23:18:48 +0530

    variable

--- php_basic.php
+++ php_basic.php
@@ -10,6 +10,27 @@
 <?php
 echo "My first PHP script!";
 ?>
+<br>
+  <!-- ___________  print __________________ -->
+<?php
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+?>
+ 
+<br>
+<!-- ___________  variable _________________ -->
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print $x + $y;
+?>
+
 
 </body>
 </html> 
